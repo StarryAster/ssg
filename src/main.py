e8d42c5a24ebde46c73afaa65d69361ccd00b5d7
Aster S. <asters@tutanota.com>
2025-04-26 22:42:29 -0300

    adaptation to github pages

--- src/main.py
+++ src/main.py
@@ -2,16 +2,17 @@ from textnode import *
 from blocktype import *
 import shutil
 import os
+import sys
 from pathlib import Path
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as file1:
         md_page = file1.read()
     with open(template_path) as file2:
         template = file2.read()
     html_page = markdown_to_html_node(md_page).to_html()
-    final_page = template.replace("{{ Title }}", extract_title(md_page)).replace("{{ Content }}", html_page)
+    final_page = template.replace("{{ Title }}", extract_title(md_page)).replace("{{ Content }}", html_page).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     
     with open(dest_path, "w") as page:
         page.write(final_page)
@@ -31,34 +32,30 @@ def copy_files(folder1, folder2):
 def fill_public():
     p = Path('.')
     # print(p.absolute())
-    if os.path.exists(os.path.join(p,"public")):
-        shutil.rmtree(os.path.join(p,"public"))
-    os.mkdir(os.path.join(p,"public"))
-    copy_files(os.path.join(p,"static"), os.path.join(p,"public"))
+    if os.path.exists(os.path.join(p,"docs")):
+        shutil.rmtree(os.path.join(p,"docs"))
+    os.mkdir(os.path.join(p,"docs"))
+    copy_files(os.path.join(p,"static"), os.path.join(p,"docs"))
     return
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_pat, basepath):
     content_list = os.listdir(dir_path_content)
     for item in content_list:
         if item.endswith(".md"):
-            generate_page(os.path.join(dir_path_content, item), template_path, os.path.join(dest_dir_path, f"{item[:-3]}.html"))
+            generate_page(os.path.join(dir_path_content, item), template_path, os.path.join(dest_dir_path, f"{item[:-3]}.html"), basepath)
         elif "." not in item:
             os.mkdir(os.path.join(dest_dir_path, item))
-            generate_pages_recursive(os.path.join(dir_path_content, item), template_path, os.path.join(dest_dir_path, item))
+            generate_pages_recursive(os.path.join(dir_path_content, item), template_path, os.path.join(dest_dir_path, item), basepath)
     return
 
 def main():
+    if len(sys.argv) < 2:
+        basepath = "/"
+    else:
+        basepath = sys.argv[1]
     fill_public()
-#    generate_page(os.path.join(Path('.'),"content", "index.md"), os.path.join(Path('.'),"template.html"), os.path.join(Path('.'), "public", "index.html"))
-#    os.mkdir(os.path.join(Path('.'), "public", "contact"))
-#    generate_page(os.path.join(Path('.'), "content", "contact", "index.md"), os.path.join(Path('.'),"template.html"), os.path.join(Path('.'), "public", "contact", "index.html"))
-#    folder_list = os.listdir(os.path.join(Path('.'), "content", "blog"))
-#    os.mkdir(os.path.join(Path('.'), "public", "blog"))
-#    for folder in folder_list:
-#        os.mkdir(os.path.join(Path('.'),"public", "blog", folder))
-#        generate_page(os.path.join(Path('.'),"content", "blog", folder, "index.md"), os.path.join(Path('.'),"template.html"), os.path.join(Path('.'), "public", "blog", folder, "index.html"))
-    generate_pages_recursive(os.path.join(Path('.'), "content"), os.path.join(Path('.'),"template.html"), os.path.join(Path('.'), "public"))
+    generate_pages_recursive(os.path.join(Path('.'), "content"), os.path.join(Path('.'),"template.html"), os.path.join(Path('.'), "docs"), basepath)
 
 
 main()
